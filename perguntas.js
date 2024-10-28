19390bffcafc29770313c7b5213232c048a97f19
joaofnc <162990103+joaofnc@users.noreply.github.com>
2024-10-28 08:31:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,46 @@
+criarCartao(
+    'Programação',
+    'O que é CSS?',
+    'O CSS é uma linguagem de estilização.'
+)
+criarCartao(
+    'Conhecimentos Gerais',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil.'
+)
+criarCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'Rússia.'
+)
+criarCartao(
+    'Língua Portuguesa',
+    'O que a palavra "legend" significa?',
+    'Significa lenda.'
+)
+criarCartao(
+    'Futebool',
+    'O Palmeiras tem mundial?',
+    'Não.'
+)
+criarCartao(
+    'Conhecimentos Gerais',
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Entre 4 a 6 litros. São retirados 450 mililitros.'
+)
+criarCartao(
+    'Animais',
+    'Qual é o animal terrestre mais rápido do mundo?',
+    'Guepardo.'
+)
+criarCartao(
+    'Conhecimentos Gerais',
+    'Quem proclamou a independência do Brasil?',
+    'Pedro Álvares Cabral.'
+)
+criarCartao(
+    "Futebool",
+    "Que País ganhou a primeira copa do mundo?",
+    "Uruguai"
+)
+
